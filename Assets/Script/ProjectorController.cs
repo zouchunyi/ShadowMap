4ccc6651f8af7aeaf810f29ab5e402782c2a5235
zouchunyi <zmafly@163.com>
2016-08-02 23:16:39 +0800

    projector

---
+++ Assets/Script/ProjectorController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectorController : MonoBehaviour {
+
+	private Camera projectorCamera = null;
+	private Projector projector = null;
+	private RenderTexture renderTexture = null;
+
+	// Use this for initialization
+	void Start () {
+	
+		projector = this.GetComponent<Projector> ();
+		InitCamera ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+
+	private void InitCamera ()
+	{
+		projectorCamera = this.gameObject.AddComponent<Camera> ();
+
+		projectorCamera.clearFlags = CameraClearFlags.SolidColor;
+		projectorCamera.backgroundColor = new Color (1, 1, 1, 0);
+		projectorCamera.fieldOfView = projector.fieldOfView;
+		projectorCamera.orthographic = projector.orthographic;
+		projectorCamera.farClipPlane = projector.farClipPlane;
+		projectorCamera.nearClipPlane = projector.nearClipPlane;
+		projectorCamera.aspect = projector.aspectRatio;
+		projectorCamera.orthographicSize = projector.orthographicSize;
+		projectorCamera.cullingMask = 1 << LayerMask.NameToLayer ("Shadow");
+		projectorCamera.depthTextureMode = DepthTextureMode.None;
+
+		renderTexture = new RenderTexture (Screen.width, Screen.height, 0);
+		renderTexture.format = RenderTextureFormat.ARGB4444;
+		renderTexture.anisoLevel = 4;
+
+		projectorCamera.targetTexture = renderTexture;
+
+		projector.material.SetTexture ("_ShadowTex", renderTexture);
+		Shader.SetGlobalMatrix ("_Projector", projectorCamera.projectionMatrix * projectorCamera.worldToCameraMatrix);
+	}
+}
---
+++ Assets/Shader/Projector.shader
@@ -0,0 +1,55 @@
+﻿Shader "Unlit/Projector"
+{
+   Properties {
+      _ShadowTex ("Projected Image", 2D) = "white" {}
+   }
+   SubShader {
+      Pass {
+         Blend One One
+         ZWrite Off
+         Offset -1, -1
+ 
+         CGPROGRAM
+ 
+         #pragma vertex vert
+         #pragma fragment frag
+ 
+         uniform sampler2D _ShadowTex;
+         uniform float4x4 _Projector;
+ 
+          struct vertexInput {
+            float4 vertex : POSITION;
+            float3 normal : NORMAL;
+         };
+         struct vertexOutput {
+            float4 pos : SV_POSITION;
+            float4 posProj : TEXCOORD0;
+         };
+ 
+         vertexOutput vert(vertexInput input)
+         {
+            vertexOutput output;
+
+            output.posProj = mul(_Projector, input.vertex);
+            output.pos = mul(UNITY_MATRIX_MVP, input.vertex);
+            return output;
+         }
+ 
+         float4 frag(vertexOutput input) : COLOR
+         {
+         	float4 col = tex2D(_ShadowTex,input.posProj.xy / input.posProj.w) * 0.1;
+
+//         	if (col.r == 1)
+//         	{
+//         		col.a = 0;
+//         	}else
+//         	{
+//         		col = float4(0,0,0,1);
+//         	}
+         	return col;
+         }
+ 
+         ENDCG
+      }
+   }
+}
